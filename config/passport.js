201d3671574d262d2d6cfe192d28bd228dd0a543
ManliestBen <benjamintmanley@gmail.com>
2020-08-22 02:54:34 -0500

    add avatar photo to user

--- config/passport.js
+++ config/passport.js
@@ -11,7 +11,12 @@ passport.use(new GoogleStrategy({
     User.findOne({ 'googleId': profile.id }, function(err, user) {
       if (err) return cb(err);
       if (user) {
-        return cb(null, user);
+        if (!user.avatar) {
+          user.avatar = profile.photos[0].value
+          user.save(function(err) {
+            return cb(null, user);
+          })
+        }
       } else {
         var newUser = new User({
           name: profile.displayName,
