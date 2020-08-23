092c3b66e7048debc0c0b0236ea577b2d2ff0b1b
ManliestBen <benjamintmanley@gmail.com>
2020-08-22 22:45:39 -0500

    add average review / split info to tabs

--- config/passport.js
+++ config/passport.js
@@ -22,7 +22,8 @@ passport.use(new GoogleStrategy({
         var newUser = new User({
           name: profile.displayName,
           email: profile.emails[0].value,
-          googleId: profile.id
+          googleId: profile.id,
+          avatar: profile.photos[0].value
         });
         newUser.save(function(err) {
           if (err) return cb(err);
