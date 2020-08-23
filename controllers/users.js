3d3122b11b0a4999e58ee27ecd884ce126020f21
ManliestBen <benjamintmanley@gmail.com>
2020-08-23 00:54:53 -0500

    update profile page functional

--- controllers/users.js
+++ controllers/users.js
@@ -5,7 +5,24 @@ module.exports = {
   index,
   show,
   addFriend,
-  removeFriend
+  removeFriend,
+  showProfile,
+  update
+}
+
+function update(req, res) {
+  User.findByIdAndUpdate(req.user._id, req.body, {new: true})
+  .then(() => {
+    res.redirect('/users/profile')
+  })
+}
+
+function showProfile(req, res) {
+  User.findById(req.user._id)
+  .populate('friends')
+  .then((user) => {
+    res.render('users/profile', { title: 'Profile Page', user })
+  })
 }
 
 function removeFriend(req, res) {
--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,11 @@ const router = require('express').Router();
 const usersCtrl = require('../controllers/users');
 
 router.get('/', isLoggedIn, usersCtrl.index);
+router.get('/profile', isLoggedIn, usersCtrl.showProfile);
 router.get('/:id', isLoggedIn, usersCtrl.show);
-router.get('/:id/friend', isLoggedIn, usersCtrl.addFriend)
-router.get('/:id/unfriend', isLoggedIn, usersCtrl.removeFriend)
-
+router.get('/:id/friend', isLoggedIn, usersCtrl.addFriend);
+router.get('/:id/unfriend', isLoggedIn, usersCtrl.removeFriend);
+router.put('/profile', isLoggedIn, usersCtrl.update)
 
 function isLoggedIn(req, res, next) {
     if ( req.isAuthenticated() ) return next();
