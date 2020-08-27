785f70ef3da092086ecf6aba0e522daaecef7096
ManliestBen <benjamintmanley@gmail.com>
2020-08-26 22:25:18 -0500

    revert to old code

--- io.js
+++ io.js
@@ -1,5 +1,4 @@
 const io = require('socket.io')()
-const axios = require('axios')
 
 
 let chatters = {}
@@ -19,31 +18,8 @@ io.on('connection', (socket) => {
   });
 
   socket.on('new_message', (data) => {
-    axios.post('https://gamegoose.herokuapp.com/users/chatroom', {
-      avatar: data.avatar,
-      username: data.username,
-      message: data.message
-    })
-    .then( () => {
-          io.sockets.emit('new_message', {message: data.message, username: data.username, avatar: data.avatar})
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+    io.sockets.emit('new_message', {message: data.message, username: data.username, avatar: data.avatar})
   })
-
-  // socket.on('new_message', (data) => {
-  //   fetch("/users/chatroom", {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/json" },
-  //     body: JSON.stringify({
-  //       avatar: data.avatar,
-  //       username: data.username,
-  //       message: data.message,
-  //     }),
-  //   });
-  //   io.sockets.emit('new_message', {message: data.message, username: data.username, avatar: data.avatar})
-  // })
 
   socket.on('typing', (data) => {
     socket.broadcast.emit('typing', {username: data.username})
